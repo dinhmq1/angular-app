2059d7c359d93d1aab9b4ac943363e1013480e40
Mike Dinh <dinhmq1@gmail.com>
2017-12-24 03:34:15 -0600

    update

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,53 +4,57 @@ var browser = require('browser-sync');
 var webpack = require('webpack-stream');
 var todo    = require('gulp-todoist');
 
-// map of paths for executing the tasks below
-
+/*
+map of paths for using with the tasks below
+ */
 var paths = {
-    entry: 'client/app/app.js',
-    app: ['client/app/**/*.{js,styl,html}'],
-    js: 'client/app/**/*!(.spec.js).js',
-    styl: 'client/app/**/*.styl',
-    toCopy: ['client/index.html'],
-    html: ['client/index.html', 'client/app/**/*.html'],
-    dest: 'dist'    
+  entry: 'client/app/app.js',
+  app: ['client/app/**/*.{js,styl,html}'],
+  js: 'client/app/**/*!(.spec.js).js',
+  styl: ['client/app/**/*.styl', 'client/style/**/*.styl'],
+  toCopy: ['client/index.html'],
+  html: ['client/index.html', 'client/app/**/*.html'],
+  dest: 'dist'
 };
 
 gulp.task('todo', function() {
-    return gulp.src(paths.js)
-      .pipe(todo({silent: false, verbose: true}));
+  return gulp.src(paths.js)
+    .pipe(todo({silent: false, verbose: true}));
 });
 
-gulp.task('build'), ['todo'], function() {
-    // Add later
-    // add entry file and build using the webpack plugin.
-    // The plugin requires webpack.config as an argument.
-    // Stream result to the destination path.
-}
+gulp.task('build', ['todo'], function() {
+  return gulp.src(paths.entry)
+    .pipe(webpack(require('./webpack.config')))
+    .pipe(gulp.dest(paths.dest));
+});
 
 gulp.task('serve', function() {
-    browser({
-      port: process.env.PORT || 4500,
-      open: false,
-      ghostMode: false,
-      server: {
-        baseDir: 'dist'
-      }
-    });
+  browser({
+    port: process.env.PORT || 4500,
+    open: false,
+    ghostMode: false,
+    server: {
+      baseDir: 'dist'
+    }
   });
-  
-// copy over required files to dist dir.
-  gulp.task('copy', function() {
-    return gulp.src(paths.toCopy, { base: 'client' })
-      .pipe(gulp.dest(paths.dest));
-  });
-  
-// watch files for new changes and execute build and copy tasks
-  gulp.task('watch', function() {
-    gulp.watch(paths.app, ['build', browser.reload]);
-    gulp.watch(paths.toCopy, ['copy']);
-  });
-  
-  gulp.task('default', function(done) {
-    sync('build', 'copy', 'serve', 'watch', done)
-  });
+});
+
+/*
+simple task to copy over needed files to dist
+ */
+gulp.task('copy', function() {
+  return gulp.src(paths.toCopy, { base: 'client' })
+    .pipe(gulp.dest(paths.dest));
+});
+
+/*
+task to watch files for changes and call build and copy tasks
+ */
+gulp.task('watch', function() {
+  gulp.watch(paths.app, ['build', browser.reload]);
+  gulp.watch(paths.toCopy, ['copy', browser.reload]);
+});
+
+gulp.task('default', function(done) {
+  sync('build', 'copy', 'serve', 'watch', done)
+});
