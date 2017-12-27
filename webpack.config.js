79b134c4cefe0558b591bf4b311644b4a90b6c44
Mike Dinh <dinhmq1@gmail.com>
2017-12-26 20:20:49 -0600

    added notes

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,11 @@ module.exports = {
     loaders: [
       { test: /\.html$/, loader: 'raw' },
       { test: /\.styl$/, loader: 'css!style!stylus' },
+      // stage=1 is a query string to pass in loader options
+      // in this case, 'stage' allows us to turn features on and off
+      // https://babeljs.io/docs/plugins/preset-stage-1/
+      // regex exclude node_modules so we don't convert everything in
+      // there from es6 to es5.
       { test: /\.js$/, loader: 'babel?stage=1', excludes: [/node_modules/] }
     ]
   },
