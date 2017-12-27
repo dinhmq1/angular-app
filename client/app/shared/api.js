bcf6b0a9bb8ac3cdb58babce4e513268d53667c9
Mike Dinh <dinhmq1@gmail.com>
2017-12-27 12:47:44 -0600

    Revert "update"
    This reverts commit 73363cddcaa7dbab7ab3703be4edfba35ca4bc69.

--- client/app/shared/api.js
+++ client/app/shared/api.js
@@ -1,6 +1,6 @@
 export const api = {
     // return fake data from json placeholder
-    url: 'http://jsonplaceholder.typicode.com'
+    url: 'https://jsonplaceholder.typicode.com'
     // un-comment to test local storage
     //url: 'http://localhost:3000'
   };
