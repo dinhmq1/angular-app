28b9b0cbb092726e374be30f0e58af257a15a147
Mike Dinh <dinhmq1@gmail.com>
2017-12-30 23:16:07 -0600

    update

--- client/app/components/blog/blog.controller.js
+++ client/app/components/blog/blog.controller.js
@@ -3,7 +3,6 @@ import _ from 'lodash';
 class BlogController {
   constructor(Posts) {
     this.message = 'Just start typing to search for posts!';
-    
     this.Posts = Posts;
     this.getPosts();
     // initiate search property because 
